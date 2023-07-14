394369e43d4224e0a11096e30adb1713b9293b0d
Mustafasohail7 <mustufasohail7@gmail.com>
2023-07-14 21:23:03 +0500

    completed anecdote

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const votes = {
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0
+  }
+   
+  const [selected, setSelected] = useState(0)
+  const [vote, setVote] = useState(votes)
+
+  const handleClick = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
+  }
+
+  const handleVote = () => {
+    const copy = {...vote}
+    copy[selected] += 1
+    setVote(copy)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <h1>Anecdote of the day</h1>
+    <div>
+      {anecdotes[selected]}
     </div>
-  );
+    <div>
+      has {vote[selected]} votes
+    </div>
+    <div>
+    <button onClick={handleVote}>
+    vote
+    </button>
+    <button onClick={handleClick}>
+    next anecdote
+    </button>
+    </div>
+    <h1>Anecdote with most votes</h1>
+    <div>
+      {anecdotes[Object.keys(vote).reduce((a, b) => vote[a] > vote[b] ? a : b)]}
+    </div>
+    <div>
+      has {vote[Object.keys(vote).reduce((a, b) => vote[a] > vote[b] ? a : b)]} votes
+    </div>
+  </>
+  )
 }
 
-export default App;
+export default App
