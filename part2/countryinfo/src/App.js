686e19ece68194f7341a09e4e4c1c24172cbda3d
Mustafasohail7 <mustufasohail7@gmail.com>
2023-07-27 16:50:07 +0500

    part 2 complete

--- part2/countryinfo/src/App.js
+++ part2/countryinfo/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
+
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    axios
+    .get(`https://studies.cs.helsinki.fi/restcountries/api/all?${countries}`)
+    .then(response => {
+      console.log(response.data)
+      setCountries(response.data)
+    })
+  },[countries])
+
+  const handleSearch = (event) => {
+    setCountries(event.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        find countries <input value={countries} onChange={handleSearch} />
+      </div>
     </div>
   );
 }
